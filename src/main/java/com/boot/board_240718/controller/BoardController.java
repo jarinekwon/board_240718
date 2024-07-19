7fd318640883b8bd6d4834be3be8d7f364546a7c
jarinekwon <kgamgak@gmail.com>
2024-07-19 17:03:01 +0900

    all files

--- src/main/java/com/boot/board_240718/controller/BoardController.java
+++ src/main/java/com/boot/board_240718/controller/BoardController.java
@@ -1,15 +1,60 @@
 package com.boot.board_240718.controller;
 
+import com.boot.board_240718.model.Board;
+import com.boot.board_240718.repository.BoardRepository;
+import jakarta.validation.Valid;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
+@Log4j2
 @Controller
 @RequestMapping("/board")
 public class BoardController {
+    @Autowired
+    private BoardRepository boardRepository;
+
     @GetMapping("/list")
     public String list(Model model) {
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards", boards);
+
         return "board/list";
+    }
+
+    @GetMapping("/form")
+//    public String form(Model model) {
+//    public String form(Model model, @RequestParam Long id) {
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        log.info("@# GetMapping form()");
+        log.info("@# id => " + id);
+
+        if(id != null) {
+//            Optional<Board> board = boardRepository.findById(id);
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+            log.info("@# board => " + board);
+        }else {
+            model.addAttribute("board", new Board());
+        }
+
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+//    public String form(@ModelAttribute Board board, Model model) {
+    public String checkPersonInfo(@Valid Board board, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
+        boardRepository.save(board);
+
+        return "redirect:/board/list";
     }
 }
