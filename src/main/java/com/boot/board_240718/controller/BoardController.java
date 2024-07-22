fe0c48de834eaa7cdc18f123a984ae7c24231f7e
jarinekwon <kgamgak@gmail.com>
2024-07-22 15:19:29 +0900

    all files

--- src/main/java/com/boot/board_240718/controller/BoardController.java
+++ src/main/java/com/boot/board_240718/controller/BoardController.java
@@ -2,9 +2,15 @@ package com.boot.board_240718.controller;
 
 import com.boot.board_240718.model.Board;
 import com.boot.board_240718.repository.BoardRepository;
+import com.boot.board_240718.validator.BoardValidator;
 import jakarta.validation.Valid;
 import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,17 +19,38 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@Log4j2
+@Slf4j
 @Controller
 @RequestMapping("/board")
 public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
 
+    @Autowired
+    private BoardValidator boardValidator;
+
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Board> boards = boardRepository.findAll();
+//    public String list(Model model) {
+//    public String list(Model model, Pageable pageable) {
+//    public String list(Model model, @PageableDefault(size = 2) Pageable pageable, @RequestParam String searchText) {
+    public String list(Model model, @PageableDefault(size = 2) Pageable pageable, @RequestParam(required = false, defaultValue = "") String searchText) {
+        log.info("@# list()");
+        log.info("@# searchText =>" + searchText);
+//        List<Board> boards = boardRepository.findAll();
+//        Page<Board> boards = boardRepository.findAll(PageRequest.of(1, 20));
+//        Page<Board> boards = boardRepository.findAll(PageRequest.of(0, 20));
+//        Page<Board> boards = boardRepository.findAll(pageable);
+        Page<Board> boards = boardRepository.findByTitleContainingOrContent(searchText, searchText, pageable);
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+//        최종: 4개 이전 페이지 표시 + 5개 이후 페이지 표시
+
+        log.info("@# boards => " + boards);
+//        boards.getTotalElements();
+
         model.addAttribute("boards", boards);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
 
         return "board/list";
     }
@@ -50,6 +77,8 @@ public class BoardController {
     @PostMapping("/form")
 //    public String form(@ModelAttribute Board board, Model model) {
     public String checkPersonInfo(@Valid Board board, BindingResult bindingResult) {
+        boardValidator.validate(board, bindingResult);
+
         if (bindingResult.hasErrors()) {
             return "board/form";
         }
